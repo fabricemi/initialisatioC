fde5c73c9abc0635bdc2f14001a55cceee815b9f
fabricemi <fabrice.bassehe@gmail.com>
2024-09-09 21:46:14 +0200

    la fonction inverse ne fonctionne pas d'abord

--- main.c
+++ main.c
@@ -3,16 +3,42 @@
 #include "operator.h"
 
 
+/*
+Pointeur:variable contenenant l'adresse memoire d'une autre variable
+
+monPointeur:adresse de la variable pointé
+*monPointeur:valeur de la variable
+&monPointeur: adresse du pointeur
+
+
+*/
+
+
+void inverser(int a, int b){
+    int tmp=b;
+    b=a;
+    a=tmp;
+
+    }
 
 int main (){
 
-recuperer();
+//recuperer();
 
 //int res=vAbs(-100,-800); genere une erreur car static
 
-int res=division(10,5);
+/* int res=division(10,5);
 
-printf("res = %d \n", res);
+printf("adresse res = %p \n", &res);
+ */
+
+
+int a=5;
+int b=7;
+
+printf("a = %d et b= %d", a, b);
+inverser(a, b);
+printf("a = %d et b= %d", a, b);
 
 return 0;
 
