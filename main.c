16767d27bfb2b66e06530565fc1532bd089bd75e
fabricemi <fabrice.bassehe@gmail.com>
2024-09-09 14:58:09 +0200

    operateur mathematique et lecteur fichier

--- main.c
+++ main.c
@@ -3,7 +3,10 @@
 
 
 int somme(int a, int b);
-
+int mul(int a, int b);
+int sous(int a, int b);
+int mod(int a, int b);
+float division(int a, int b);
 int main (){
 
 int a=0;
@@ -14,7 +17,11 @@ scanf("%d", &a);
 printf("saisir b :");
 scanf("%d", &b);
 
-printf("res= %d \n", somme(a, b));
+printf("A + B= %d /n", somme(a, b));
+printf("A - B= %d /n", sous(a, b));
+printf("A * B= %d /n", mul(a, b));
+printf("A / B= %f /n", division(a, b));
+printf("A mod B= %d /n", mod(a, b));
 
 
 return 0;
@@ -26,4 +33,17 @@ int somme(int a, int b){
 return a+b;
 }
 
+int mul(int a, int b){
+return a*b;
+}
 
+int sous(int a, int b){
+return a-b;
+}
+
+float division(int a, int b){
+return a/b;}
+
+int mod(int a, int b){
+    return a%b;
+}
